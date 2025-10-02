c8232683685990649c6bdb831bbd8341f7b50e7a
MarkGhebrial <mghebrialjr@icloud.com>
2025-10-01 18:52:47 -0700

    Get events from discord server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::all::{Guild, Shard};
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+const GUILD_ID: u64 = 1224949123141210173;
+
+struct Handler;
+
+async fn generate_invite(client: Client) {
+    let guild = serenity::model::id::GuildId::new(GUILD_ID);
+
+    // guild.invites(client.http).await.unwrap()
+
+    todo!();
+}
+
+#[tokio::main]
+async fn main() {
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_SCHEDULED_EVENTS | GatewayIntents::GUILD_INVITES;
+
+    let client_builder = Client::builder(&token, intents);
+
+    // let application_id = client_builder.get_application_id().unwrap();
+    // application_id
+
+    let client = client_builder.await.unwrap();
+
+    let guild = serenity::model::id::GuildId::new(GUILD_ID);
+
+    // let server_name = guild.name(client.cache).unwrap();
+    // println!("Server name is '{server_name}'");
+
+    let events = guild.scheduled_events(client.http, true).await.unwrap();
+
+    for event in events {
+        println!("Event name: {}", event.name);
+        println!("Event time: {} - {:?}", event.start_time, event.end_time);
+        println!("Event location: {:?}", event.metadata.unwrap().location);
+        println!("Number of RSVPs: {:?}", event.user_count);
+
+        println!("Description: {:?}", event.description)
+        // println!("{event:?}");
+    }
+
+    // client.
+}
+
+struct EventDetails {
+    name: Option<String>,
+    start_time: (),
+    end_time: (),
+    location: Option<String>,
+    rsvps: usize,
 }
---
+++ www/content/_index.md
@@ -0,0 +1,50 @@
++++
+template = "index.html"
++++
+
+# Heading 1
+
+## Heading 2
+
+### Heading 3
+
+#### Heading 4
+
+##### Heading 5
+
+###### Heading 6
+
+# About Us
+
+San Diego Nerf Club.
+
+We typically host one or two games per month. Our competitive games are either hosted
+in a church gym or at the Huntington Beach Sports Complex. Our casual games typically
+happen monthly at Modern Airsoft in Oceanside and semiannually at Jungle Island
+Paintball.
+
+You can view our current event schedule [here](/events). For more information about
+the club and our events, visit our discord server.
+
+# Social Media
+
+We are most active on our Discord server. You can join by clicking [here](/discord).
+
+We also have a Facebook group, but it's an ancient relic that's no longer active.
+
+# Other SoCal Nerf Clubs
+
+## Lazy Uncles' Nerf Club
+
+LUNC is the largest club in SoCal. They host monthly games at parks in the Riverside/
+Redlands area. Their games are very casual and tend to attract families and children.
+
+The LUNC facebook group is the best way to keep track of their events.
+
+## Small Group Nerf War
+
+SGNW is a small club that plays monthly at a park in Lakewood. They typically play
+4v4, 5v5, and 6v6 semi-competetive game modes depending on attendance.
+
+
+
